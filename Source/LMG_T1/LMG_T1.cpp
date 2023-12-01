63a1773bf7f32594a8bb10f8e7098107a7043cc5
geniuslm <geniuslm1227@gmail.com>
2023-12-01 22:47:45 +0800

    初始化

---
+++ Source/LMG_T1/LMG_T1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LMG_T1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LMG_T1, "LMG_T1" );
